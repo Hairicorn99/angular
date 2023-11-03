646e00d9136b28088b6bb8d48d82afb018ee8e8c
Hairicorn99 <mdhaic5tantrao2018@gmail.com>
2023-11-03 15:06:34 +0700

    product-detail-ui update

--- src/app/proxy/index.ts
+++ src/app/proxy/index.ts
@@ -1,5 +1,6 @@
 import * as AiProStore from './ai-pro-store';
+import * as Manufacturers from './manufacturers';
 import * as ProductCategories from './product-categories';
 import * as Products from './products';
 export * from './models';
-export { AiProStore, ProductCategories, Products };
+export { AiProStore, Manufacturers, ProductCategories, Products };
